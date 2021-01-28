d4cc9a3b653e8de0a58acac8d86160df30045a29
Baiyixuan1113 <61569900+Baiyixuan1113@users.noreply.github.com>
2021-01-28 14:14:28 +0800

    Update and rename data_divide_new.py to data_divide.py

--- code/data_divide.py
+++ code/data_divide.py
@@ -8,8 +8,8 @@ num_people = 1050  # 数据人数
 num_original = 50  # 正样本数量
 num_forgeries = 50  # 负样本数量
 
-dataset_path = '/home/data/Datasets/HWDSV_data/HWDS_20201225'
-save_path = '/home/data/ETOP/project_IDN/data'
+dataset_path = '/home/data/Datasets/HWDSV_data'
+save_path = '/home/DeepLeaning/project_IDN/data'
 train_data_file = 'train_data.csv'
 val_data_file = 'val_data.csv'
 test_data_file = 'test_data.csv'
@@ -41,69 +41,17 @@ def combine_2list(list1, list2):
     return answers
 
 
-def generate_df(train_size):
+def generate_df(num_people):
+    train_size = int(num_people*0.9)
+    val_size = int(num_people*0.08)
+    test_size = num_people-train_size-val_size
+    
     signature_list = list(range(1, num_people+1))
-    train_indexs = np.arange(1, len(signature_list)+1, 1)
-    np.random.shuffle(train_indexs)
-    train_indexs = train_indexs[:train_size + 1]
-
-    #########################################
-    # 按组别划分数据集
-    # a = [1, 22, 43, 64, 85,
-    #      106, 127, 148, 169, 190,
-    #      211, 232, 253, 274, 295,
-    #      316, 337, 358, 379, 400,
-    #      421, 442, 463, 484, 505,
-    #      526, 547, 568, 589, 610,
-    #      631, 652, 673, 694, 715,
-    #      736, 757, 778, 799, 820,
-    #      841, 862, 883, 904, 925,
-    #      946, 967, 988, 1009, 1030]
-    # b = random.sample(a, 49)
-    # c = list(set(a).difference(set(b)))
-    # d = random.sample(b, 45)
-    # e = list(set(b).difference(set(d)))
-
-    # train_nums = d
-    # val_nums = e
-    # test_nums = c
-    # train_nums.sort()
-    # val_nums.sort()
-    # test_nums.sort()
-    #########################################
-    # 1050组按组别划分数据集
-    lis_a = list(range(50))
-    lis_aa = [a*21+1 for a in lis_a]
-    lis_b = list(range(13, 35))
-    lis_bb = [b*21+1 for b in lis_b]
-
-    lis_test = random.sample(lis_bb, 1)
-    # 求差集，在lis_aa中但不在lis_test中
-    lis_trainval = list(set(lis_aa).difference(set(lis_test)))
-    lis_train = random.sample(lis_trainval, 45)
-    lis_val = list(set(lis_trainval).difference(set(lis_train)))
-    lis_train.sort()
-    lis_val.sort()
-    lis_test.sort()
-    #########################################
-
-    train_indexs = []
-    val_indexs = []
-    test_indexs = []
-    for xx in lis_train:
-        for yy in range(21):
-            train_indexs.append(xx+yy)
-    for xx in lis_val:
-        for yy in range(21):
-            val_indexs.append(xx+yy)
-    for xx in lis_test:
-        for yy in range(21):
-            test_indexs.append(xx+yy)
-    print('len_train:', len(train_indexs))
-    print('len_val:', len(val_indexs))
-    print('len_test:', len(test_indexs))
-    #########################################
-
+    lis_train = random.sample(signature_list, train_size)
+    lis_val_test = list(set(signature_list).difference(set(lis_train)))
+    lis_val = random.sample(lis_val_test, val_size)
+    lis_test = list(set(lis_val_test).difference(set(lis_val)))
+    
     org_path = os.path.join(dataset_path, 'full_org', 'original_')
     forg_path = os.path.join(dataset_path, 'full_forg', 'forgeries_')
 
@@ -174,7 +122,7 @@ def generate_df(train_size):
 
     f_train.close()
     f_val.close()
-
+    f_test.closer()
 
 if __name__ == "__main__":
-    generate_df(train_size=945)
+    generate_df(num_people)
